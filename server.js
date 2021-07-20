daf3bbedf608824a40becabbe0eaaeea81bccfbf
Tatsuki Yamantani <tatsuki.y@wsad.io>
2021-07-20 16:35:09 -0700

    no message

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const favicon = require("express-favicon");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
 const path = require("path");
-const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.ico"));
+const port = process.env.PORT || 8080;
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-app.listen(port);
+
+// This route serves the React app
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "build", "index.html"))
+);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
