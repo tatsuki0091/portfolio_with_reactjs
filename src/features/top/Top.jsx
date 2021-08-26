1728035856c4f4188f76eb8f8a15aa3c65424987
tatsuki0091 <rossosalvatore@icloud.com>
2021-08-26 15:21:22 -0700

    Change pdf

--- src/features/top/Top.jsx
+++ src/features/top/Top.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import "./top.css";
-import About from "../about/About";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import './top.css';
+import About from '../about/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Container } from "react-bootstrap";
 // import helloImage from "../../images/hello.jpeg";
-import Header from "../common/Header";
-import Introduction from "../introduction/Introduction";
+import Header from '../common/Header';
+import Introduction from '../introduction/Introduction';
 // import Fadein from "../animation/Fadein";
-import { Email } from "../mail/Email";
-import Work from "../work/Work";
-import { Animated } from "react-animated-css";
+import { Email } from '../mail/Email';
+import Work from '../work/Work';
+import { Animated } from 'react-animated-css';
 const Top = () => {
   return (
     <div>
